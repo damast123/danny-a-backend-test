c1fabdd594826bab0aaab0a4f564fba5be5a60b6
damast123 <36781247+damast123@users.noreply.github.com>
2025-06-14 14:26:06 +0700

    Generate Excel
    This will generate excel:
    - Import laravel excel
    - Add export and api
    - Update controller and model

--- app/Models/TodoList.php
+++ app/Models/TodoList.php
@@ -31,4 +31,48 @@ class TodoList extends Model
     {
         return $query->where('priority', $priority);
     }
+
+    public static function getTodoList()
+    {
+        return self::select('title', 'assignee', 'due_date', 'time_tracked', 'status', 'priority')
+            ->orderBy('due_date', 'desc');
+    }
+
+    public static function getFilteredTodoList($filters)
+    {
+        return self::getTodoList()->filtered($filters)->get();
+    }
+
+    public function scopeFiltered($query, $filters)
+    {
+        if (!empty($filters['title'])) {
+            $query->where('title', 'ilike', '%' . $filters['title'] . '%');
+        }
+
+        if (!empty($filters['assignee'])) {
+            $assignees = explode(',', $filters['assignee']);
+            $query->whereIn('assignee', $assignees);
+        }
+
+        if (!empty($filters['due_start']) && !empty($filters['due_end'])) {
+            $query->whereBetween('due_date', [$filters['due_start'], $filters['due_end']]);
+        }
+
+        if (!empty($filters['time_min']) && !empty($filters['time_max'])) {
+            $query->whereBetween('time_tracked', [(int)$filters['time_min'], (int)$filters['time_max']]);
+        }
+
+        if (!empty($filters['status'])) {
+            $status_todo = explode(',', $filters['status']);
+            $query->whereIn('status', $status_todo);
+        }
+
+        if (!empty($filters['priority'])) {
+            $priority_todo = explode(',', $filters['priority']);
+            $query->whereIn('priority', $priority_todo);
+        }
+
+        return $query;
+    }
+
 }
--- app/Services/TodoListService.php
+++ app/Services/TodoListService.php
@@ -5,9 +5,20 @@
 
   class TodoListService
   {
+      /**
+       * Check if the request due date is late.
+       *
+       * @param string $request_due_date
+       * @return bool
+       */
       public function isDateLate($request_due_date): bool
       {
           $date = Carbon::createFromFormat('Y-m-d', $request_due_date);
           return $date < Carbon::now();
+      }
+      public function setDate()
+      {
+          $date = Carbon::now();
+          return $date->format('Y-m-d-H:i:s');
       }
   }
